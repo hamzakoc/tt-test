dbc9b0bc10cde7bcbb37d74d79b641a4fefad09f
Hamza Koc <hamzakooc@gmail.com>
2023-04-27 13:49:04 -0400

    first commit

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
 resource "azurerm_resource_group" "rg6" {
   name     = "rg-new-6"
   location = "Canada Central"
-  tags = {
-    env    = "dev"
-    owner  = "Kenan Bey"
-    owner2 = "Burak Bey"
-  }
 }
 
-
-module "myrg" {
-  source      = "./modules/rg"
-  rg_name     = var.myrgname
-  rg_location = var.myrglocation
-}
 
